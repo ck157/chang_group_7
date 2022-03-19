6dce70623ac4cc58e2a7e9e1c347c3a3ded1caf9
SONTAEGUK <101920873+SONTAEGUK@users.noreply.github.com>
2022-03-19 18:00:14 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,6 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  get selectedItemColor => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +36,12 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text("쉿! 비밀장부"),
         backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white),
+            onPressed: () {},
+          )
+        ],
       ),
       body: Column(children: <Widget>[
         Padding(
@@ -75,6 +84,63 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart),
+            label: '경조사비',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: '채무관계',
+            backgroundColor: Colors.green,
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+//네비게이터는 바디를 2개로 만들어주는 뭔가 아래 같은거를 써야할꺼 같음
+
+/*
+class _MisoState extends State<Miso> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex, // index 순서에 해당하는 child를 맨 위에 보여줌
+        children: [
+          Page1(),
+          page2(),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex, // 현재 보여주는 탭
+        onTap: (newIndex) {
+          print("selected newIndex : $newIndex");
+          setState(() {
+            // 보여주려는 index 변경
+            currentIndex = newIndex;
+          });
+        },
+        selectedItemColor: Colors.white, // 선택된 아이콘 색상
+        unselectedItemColor: Colors.white, // 선택되지 않은 아이콘 색상
+        showSelectedLabels: false, // 선택된 항목 label 숨기기
+        showUnselectedLabels: false, // 선택되지 않은 항목 label 숨기기
+        type: BottomNavigationBarType.fixed, // 선택시 아이콘 움직이지 않기
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(Text("경조사비", textScaleFactor: 1.5),
+          BottomNavigationBarItem(Text("채무관계", textScaleFactor: 1.5), */
